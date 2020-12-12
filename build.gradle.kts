cb4578badb075bffe68434a85debfec630c8bc7f
guynir <guynir75@gmail.com>
2020-12-12 09:55:44 +0200

    Add Gradle support for publishing artifact
    Publishing is done via Maven publishing plugin for Gradle
    using current actor (repository's owner username) as the username
    and GitHub private access token (taken from repository's secretes).

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,42 @@
+import org.gradle.internal.logging.text.StyledTextOutput
+import org.gradle.kotlin.dsl.support.serviceOf
+
 plugins {
     id("java")
+    id("maven-publish")
 }
 
-group = "io.storage"
-version = "storage"
+/**
+ * Maintains artifact's properties (group, name, version).
+ */
+object Artifact {
+    const val GROUP_ID = "io.storage"
+    const val NAME = "core"
+    const val VERSION = "1.0.0"
+}
+
+/**
+ * Maintains the list of properties required for authorization.
+ */
+object AuthProperties {
+    const val USERNAME_PROPERTY_NAME = "GITHUB_ACTOR"
+    const val PASSWORD_PROPERTY_NAME = "GITHUB_TOKEN"
+}
+
+// Logger helper for styling output (e.g.: writing message in bold/color).
+val styledOutput: StyledTextOutput = project
+        .serviceOf<org.gradle.internal.logging.text.StyledTextOutputFactory>()
+        .create("styledOutput")
+
+//
+// Artifact properties.
+//
+group = Artifact.GROUP_ID
+version = Artifact.VERSION
+
+//
+// Java compiler options.
+//
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -41,6 +74,66 @@ dependencies {
 
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    //
+    // Before running publishing procedure, make sure we have everything we need.
+    //
+    publish {
+        if (!hasVariable(AuthProperties.USERNAME_PROPERTY_NAME) || !hasVariable(AuthProperties.PASSWORD_PROPERTY_NAME)) {
+            styledOutput
+                    .style(StyledTextOutput.Style.FailureHeader)
+                    .println("Error: Missing either '${AuthProperties.USERNAME_PROPERTY_NAME}' or " +
+                            "'${AuthProperties.PASSWORD_PROPERTY_NAME}' system properties required for " +
+                            "publishing an artifact.")
+
+            throw GradleException("Missing authentication properties.")
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/NocWriter/storageio")
+            credentials {
+                username = getVariable(AuthProperties.USERNAME_PROPERTY_NAME)
+                password = getVariable(AuthProperties.PASSWORD_PROPERTY_NAME)
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = Artifact.GROUP_ID
+            artifactId = Artifact.NAME
+            version = Artifact.VERSION
+
+            from(components["java"])
+        }
+    }
+}
+
+/**
+ * Fetch property value from Java system properties (first priority) or environment variables (second priority).
+ *
+ * @param name Name of property.
+ * @return Property value or {@code null} if property is undefined.
+ */
+fun getVariable(name: String): String? {
+    return System.getProperty(name, null) ?: System.getenv(name)
+}
+
+/**
+ * Check if a system property/environment variable is defined.
+ *
+ * @param name Name of property.
+ * @return {@code true} if property is defined, {@code false} if not.
+ */
+fun hasVariable(name: String): Boolean {
+    return getVariable(name) != null
 }
